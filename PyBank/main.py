584a5c64398ce9e18759a26ff4bb61fb07bff92f
Lake Brant <lake.brant@du.edu>
2020-10-02 12:37:30 -0600

    Profit/Loss Difference first try

--- PyBank/main.py
+++ PyBank/main.py
@@ -34,6 +34,26 @@ with open(budget_data, "r") as csvfile:
     net_profloss = sum([int(row[1]) for row in reader])
     print(f"Total: ${net_profloss}")
 
+# Average changes in Profit/Loss
+
+with open(budget_data, "r") as csvfile:
+    reader = csv.reader(csvfile, delimiter = ',')
+    next(reader)
+
+    diff_proffloss = 0
+    i = 0
+
+    for row in reader:
+       i += 1
+       diff_proffloss = (float(row[i]) - float(row[i - 1])) 
+    
+    print(diff_proffloss)
+
+
+    
+
+
+
 
 
 
