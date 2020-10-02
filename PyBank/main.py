e3eb950fdf0e04081f42d2f283b15995294acc8d
Lake Brant <lake.brant@du.edu>
2020-10-02 14:13:16 -0600

    greatest increase and decrease final

--- PyBank/main.py
+++ PyBank/main.py
@@ -18,6 +18,8 @@ total_months = []
 
 budget_data = os.path.join("Resources", "budget_data.csv")
 
+print('Financial Analysis')
+print('---------------------')
 
 # Open the csv file and store the header 
 
@@ -43,12 +45,12 @@ with open(budget_data, "r") as csvfile:
     # create lists for values to be stored in. One for the profit/loss column and one for the difference
     diff_proffloss = []
     proffloss = []
-    loss_date = []
+    date = []
 
     # append column list to contain all the original values
     for row in reader:
         proffloss.append(float(row[1]))
-        loss_date.append(row[0])
+        date.append(row[0])
     
 
     # append the difference list to contain new values
@@ -62,16 +64,39 @@ with open(budget_data, "r") as csvfile:
     # print and round result to the nearest two decimal places
     print(f"Average Profit/Loss: ${round(average_profloss, 2)}")
 
-    # find the greatest increase and decrease in profits
-    profit_loss = min(diff_proffloss)
-    profit_gain = max(diff_proffloss)
+    # find values for greatest increase and decrease in profits
+    profit_loss = round(min(diff_proffloss))
+    profit_gain = round(max(diff_proffloss))
 
-    # loss_date = []
-    for col in reader:
-        if float(col[1]) == profit_loss:
-            loss_date.append(str(col[0]))
+    # find corressponding indecies 
+    ind_loss = diff_proffloss.index(profit_loss)
+    ind_gain = diff_proffloss.index(profit_gain)
+
+    # find date in the date list with correct index. Have to add 1 because we have 1 less piece of data in that list compared to master list
+    loss_date = date[ind_loss + 1]
+    gain_date = date[ind_gain + 1]
     
-    print(loss_date)
+    # print both statements
+    print(f'Greatest Increase in Profits: {gain_date} (${profit_gain})')
+    print(f'Greatest Decrease in Profits: {loss_date} (${profit_loss})')
+    
+
+    # find the greatest increase and decrease in profits
+
+    #loss_date = str(date[proffloss.index(profit_loss)])
+    
+    #for row in reader:
+        #if row[1] == float(profit_loss):
+            #print(row[0])
+        #else:
+           # print('your doing it wrong, dumbass')
+
+           # break
+    
+
+
+
+    
 
 
 
